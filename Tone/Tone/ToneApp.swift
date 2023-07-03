55195fdc578ace8223e8aed5341db1f46adb9f85
Даниил Ермолаев <daniilermolaev@MacBook-Pro-Daniil.local>
2023-07-03 14:45:53 +0500

    Initial XCODE appliaction

---
+++ Tone/Tone/ToneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneApp.swift
+//  Tone
+//
+//  Created by Даниил Ермолаев on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
